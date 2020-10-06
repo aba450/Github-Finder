8ee2df3c412907d1760ecb2ab9952727baf90584
aba450 <54678566+aba450@users.noreply.github.com>
2020-10-06 02:09:57 -0400

    context api and final commit before deploy

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,35 +1,28 @@
-import React from 'react'
-import UserItem from './UserItem'
-import Spinner from '../layout/Spinner'
-import PropTypes from 'prop-types'
+import React, { useContext } from "react";
+import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
+import GithubContext from "../../context/github/githubContext";
 
+const User = () => {
+  const githubContext = useContext(GithubContext);
+  const { loading, users } = githubContext;
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div style={userStyle}>
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
+};
 
-const User = ({ users, loading}) => {
-
-    if (loading){
-        return <Spinner />
-    }else{
-        return (
-            <div style = {userStyle}>
-               {users.map(user => (
-                   <UserItem key = {user.id} user={user} />
-               ))} 
-            </div>
-        )
-
-    }
-}
-
-User.propTypes = {
-    users : PropTypes.array.isRequired,
-    loading : PropTypes.bool.isRequired,
-
-}
 const userStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(3, 1fr)',
-    gridGap: '1rem'
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
+  gridGap: "1rem",
+};
 
-}
-
-export default User
+export default User;
